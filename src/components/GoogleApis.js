adecee6704a4f088a1a05536e4c760680fe797a4
nitin-codr <nitinporwal99@gmail.com>
2020-05-14 19:55:37 +0530

    authentication added

--- src/components/GoogleApis.js
+++ src/components/GoogleApis.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 
 class GoogleApis extends Component {
+    state = {
+        isSignedIn: null
+    }
     componentDidMount = () => {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId: "1031542362334-j5sobbh87osg7r5ulc8do8vjefo02320.apps.googleusercontent.com",
                 scope: "email"
-            })
-        })
+            }).then(()=> {
+                this.auth=window.gapi.auth2.getAuthInstance();
+                this.setState({isSignedIn: this.auth.isSignedIn.get()});
+            });
+        });
+    };
+
+    renderAuthButton = () => {
+        if(this.state.isSignedIn===null) {
+            return <div>I dont know if we are signed in or not</div>
+        }
+        else if(this.state.isSignedIn) {
+            return <div>I am signed in!</div>
+        }
+        else {
+            return <div>I am not signed in </div>
+        }
     }
     render() {
         return (
             <div>
-                GoogleApis
+                {this.renderAuthButton()}
             </div>
         );
     };
