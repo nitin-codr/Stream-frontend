63b1818a090e57a1e67f1bc214c5488e72d460ac
nitin-codr <nitinporwal99@gmail.com>
2020-05-14 17:00:06 +0530

    simple demo on route component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,32 @@
 import React, {Component} from 'react';
+import {BrowserRouter, Route} from 'react-router-dom'
+
+const PageOne = () => {
+    return (
+        <div>
+            Page One
+        </div>
+    )
+}
+const PageTwo = () => {
+    return (
+        <div>
+            Page Two
+        </div>
+    )
+}
 
 class App extends Component {
     render() {
         return (
             <div>
-                App
+                <BrowserRouter >
+                    <div>
+                        <Route path="/" exact component={PageOne} />
+                        <Route path="/pagetwo" component={PageTwo} />
+                    </div>
+                
+                </BrowserRouter>
             </div>
         )
     };
