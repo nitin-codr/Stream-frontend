51d750d2d8a35bf72808ee4f85f6713b4bd48780
nitin-codr <nitinporwal99@gmail.com>
2020-05-14 18:19:35 +0530

    wired up routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,10 @@
 import React, {Component} from 'react';
-import {BrowserRouter, Route, Link} from 'react-router-dom'
-
-const PageOne = () => {
-    return (
-        <div>
-            Page One
-            <Link to="/pagetwo">Switch to page two</Link>
-        </div>
-    )
-}
-const PageTwo = () => {
-    return (
-        <div>
-            Page Two
-            <Link to="/">Switch to page one</Link>
-        </div>
-    )
-}
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import StreamList from './streams/StreamList';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamShow from './streams/StreamShow';
 
 class App extends Component {
     render() {
@@ -24,10 +12,13 @@ class App extends Component {
             <div>
                 <BrowserRouter >
                     <div>
-                        <Route path="/" exact component={PageOne} />
-                        <Route path="/pagetwo" component={PageTwo} />
+                        <Route path="/" exact component={StreamList} />
+                        <Route path="/streams/new" component={StreamCreate} />
+                        <Route path="/streams/edit" component={StreamEdit} />
+                        <Route path="/streams/delete" component={StreamDelete} />
+                        <Route path="/streams/show" component={StreamShow} />
                     </div>
-                
+
                 </BrowserRouter>
             </div>
         )
