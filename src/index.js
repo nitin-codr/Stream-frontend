9526651d34c60c8b9ce67dba0a9bdacb193d8319
nitin-codr <nitinporwal99@gmail.com>
2020-05-15 01:26:57 +0530

    added redux devtools

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {createStore, compose, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store=createStore(reducers);
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store=createStore(reducers, composeEnhancer(applyMiddleware()));
 ReactDOM.render(
     <Provider store={store} >
         <App />
