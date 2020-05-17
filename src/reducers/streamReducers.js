c92cd2127573a2f88528660d6978d89eb7d20653
nitin-codr <nitinporwal99@gmail.com>
2020-05-18 01:55:14 +0530

    bulk of reducers added

---
+++ src/reducers/streamReducers.js
@@ -0,0 +1,19 @@
+import { CREATE_STREAM, EDIT_STREAM, FETCH_STREAMS, FETCH_STREAM, DELETE_STREAM } from "../actions/types";
+
+import _ from 'lodash';
+const INITIAL_STATE={};
+
+export default (state=INITIAL_STATE, action) => {
+    switch(action.type) {
+        case CREATE_STREAM:
+            return {...state, [action.payload.id]: action.payload};
+        case EDIT_STREAM:
+            return {...state, [action.payload.id]: action.payload};
+        case FETCH_STREAM:
+            return {state, [action.payload.id]: action.payload};
+        case FETCH_STREAMS:
+            return _.mapKeys(action.payload, 'id');
+        case DELETE_STREAM:
+            return _.omit(state, [action.payload]);
+    }
+}
