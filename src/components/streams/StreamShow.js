2822c0fd9e6b58fc20057f1c5bdad1e41829dac3
nitin-codr <nitinporwal99@gmail.com>
2020-05-19 23:05:32 +0530

    show stream link setup

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -7,7 +7,6 @@ class StreamShow extends Component {
         this.props.fetchStream(this.props.match.params.id);
     }
     render() {
-        console.log(this.props.stream);
         return (
             <div>
                 {this.props.stream.title}
