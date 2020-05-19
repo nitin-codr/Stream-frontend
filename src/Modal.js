38beb8d8c3086e6e5af94470f8b9a8ad7577a924
nitin-codr <nitinporwal99@gmail.com>
2020-05-19 21:37:30 +0530

    added styles to modal

--- src/Modal.js
+++ src/Modal.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import history from './history';
 
 const Modal = () => {
     return ReactDOM.createPortal(
-        <div className="ui dimmer modals visible active">
-            <div className="ui standatd modal visible active">
-                Coming soon!!
+        <div onClick={() => history.push('/')} className="ui dimmer modals visible active">
+            <div onClick={(e) => e.stopPropagation()} className="ui standatd modal visible active">
+                <div className="header">
+                    Delete Stream
+                </div>
+                <div className="content">
+                    Are you sure you wnat to delete this stream?
+                </div>
+                <div className="actions">
+                    <button className="ui button negative">Delete</button>
+                    <button className="ui button">Cancel</button>
+                </div>
             </div>
         </div>,
         document.querySelector('#modal')
